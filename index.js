f3cc78bc08e246c206665ee0e1db3fac0c54f814
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-08-30 00:16:16 +0000

    Some additional work in the index.js done

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ window.onload = () => {
     let randomNumber = Math.random () *10;
     document.querySelector (#excuse) inner HTML =randomNumber;
 }
+
+let generateExcuse = () => {
+    let pronoun [];
+    let subject [];
+    let verb [];
+    let place [];
+    return "My dog ate my homework"
+};
