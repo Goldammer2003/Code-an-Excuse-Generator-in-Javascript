becae392f2ff23ec493ea8acd7b3baebe6ad0ce5
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-08-30 00:10:22 +0000

    Some work done inside of index.js

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ import "bootstrap";
 import '../style/index.scss';
 
 
-window.onload = () -> {
-    let randomNumber = Math.random () * 10;
-console.log ('Hello Manuel from the console!';
-)
+window.onload = () => {
+    let randomNumber = Math.random () *10;
+    document.querySelector (#excuse) inner HTML =randomNumber;
 }
-    
