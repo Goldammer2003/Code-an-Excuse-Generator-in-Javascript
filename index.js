85a5f34556435950df66e93a6cb126391c608bb1
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-08-30 00:29:12 +0000

    Added the let place in the let gererate Excuse

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ window.onload = () => {
 }
 
 let generateExcuse = () => {
-    let pronoun "The",];
-    let subject ['cat', 'dog' 'my brother' 'rabbit'];
-    let verb [ "took my" ,"ate my", "stole my" ];
-    let place "in my house" ,"in my garden" ,"in my room" ,"in my backjard"];
+    let pronoun = "The",];
+    let subject = ['cat', 'dog' 'my brother' 'rabbit'];
+    let verb =  [ "took my" ,"ate my", "stole my" ];
+    let place =  "in my house" ,"in my garden" ,"in my room" ,"in my backjard"];
+    let item = ["assigment" "homework" "keys" ]
     return "My dog ate my homework"
 };
