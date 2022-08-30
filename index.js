5740c6e04183e4fad3f75ccbe15eaebf79d21db9
Manuel Alexander Goldammer <mangobo@hotmail.de>
2022-08-30 00:01:29 +0000

    Deleded a toubled index.html file/ styles.css file

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import "bootstrap";
 import '../style/index.scss';
 
 
-window.onload = function () {
-    console.log
+window.onload = () -> {
+    let randomNumber = Math.random () * 10;
+console.log ('Hello Manuel from the console!';
+)
 }
+    
